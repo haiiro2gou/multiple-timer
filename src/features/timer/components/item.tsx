a9238929aa99ae8ca53b881e610d114884d885a7
はいいろ <mrashen.2gou@gmail.com>
2025-09-18 01:51:19 +0900

    style: improve 3-dots

--- src/features/timer/components/item.tsx
+++ src/features/timer/components/item.tsx
@@ -111,12 +111,12 @@ export const TimerItem = ({ timer, currentTime }: TimerItemProps) => {
                         fill="none"
                         viewBox="0 0 24 24"
                         stroke="currentColor"
+                        strokeWidth={2}
                     >
                         <path
                             strokeLinecap="round"
                             strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                            d="M12 5v.01M12 12v.01M12 19v.01"
                         />
                     </svg>
                 </button>
