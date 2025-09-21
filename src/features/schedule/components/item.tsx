d065b04652b7e4a418140ce7cfb545787017ef48
はいいろ <mrashen.2gou@gmail.com>
2025-09-22 04:49:21 +0900

    fix: correct reverse placement of pause / resume button

--- src/features/schedule/components/item.tsx
+++ src/features/schedule/components/item.tsx
@@ -181,7 +181,7 @@ export const ScheduleItem = ({
                             >
                                 <path
                                     fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+                                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z"
                                     clipRule="evenodd"
                                 />
                             </svg>
@@ -202,7 +202,7 @@ export const ScheduleItem = ({
                             >
                                 <path
                                     fillRule="evenodd"
-                                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
                                     clipRule="evenodd"
                                 />
                             </svg>
