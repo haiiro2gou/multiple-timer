26d69d9b2c83f0cb08e67211d9d32f05249525f1
はいいろ <mrashen.2gou@gmail.com>
2025-09-30 10:31:45 +0900

    build: setup for github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
 import * as React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import { App } from "./App.tsx";
 import { ThemeProvider } from "./features/theme-provider.tsx";
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="light"
-            enableSystem
-            disableTransitionOnChange
-        >
-            <App />
-        </ThemeProvider>
+        <BrowserRouter basename="/multiple-timer">
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="light"
+                enableSystem
+                disableTransitionOnChange
+            >
+                <App />
+            </ThemeProvider>
+        </BrowserRouter>
     </React.StrictMode>
 );
